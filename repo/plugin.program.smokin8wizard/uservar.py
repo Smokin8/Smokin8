e44d21051c513e88b1229a0ac4c913391f7088de
Patrick <therealrusha92@gmail.com>
2021-08-01 07:52:37 -0500

    Build

--- repo/plugin.program.smokin8wizard/uservar.py
+++ repo/plugin.program.smokin8wizard/uservar.py
@@ -17,7 +17,7 @@ ADDONTITLE = '[COLOR limegreen][B]Smokin8[/B][/COLOR]Wizard'
 BUILDERNAME = 'Smokin8Wizard'
 EXCLUDES = [ADDON_ID, 'repository.openwizard']
 # Text File with build info in it. Please read https://github.com/a4k-openproject/plugin.program.openwizard/wiki/Installing-Builds
-BUILDFILE = 'http://'
+BUILDFILE = 'https://raw.githubusercontent.com/Smokin8/build-files/main/builds.txt'
 # How often you would like it to check for build updates in days
 # 0 being every startup of kodi
 UPDATECHECK = 0
